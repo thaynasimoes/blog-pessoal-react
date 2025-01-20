bb8b5b744b212bf327df74485b196d3bf2f44c91
Thayná Simões <thayna.pereira2402@gmail.com>
2025-01-20 08:08:59 -0300

    Adicionado contexto e lógica de autenticação.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
+
   return (
-    <>
-        <BrowserRouter>
-          <Navbar />
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-  )
+      <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      </AuthProvider>
+  );
 }
 
-export default App
+export default App;
