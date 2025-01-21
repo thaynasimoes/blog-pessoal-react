e9269d28b8e951733119dc15503dfcae6877c958
Thayná Simões <thayna.pereira2402@gmail.com>
2025-01-20 21:20:37 -0300

    Implementação da lógica de exclusão de tema no componente DeletarTema

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import { AuthProvider } from './contexts/AuthContext';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/temas/listatemas/ListaTemas";
+import FormTema from "./components/temas/formtema/FormTema";
+import DeletarTema from "./components/temas/deletartema/DeletarTema";
 
 function App() {
-
   return (
-      <AuthProvider>
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/temas" element={<ListaTemas />} />
+          <Route path="/cadastrartema" element={<FormTema />} />
+          <Route path="/editartema/:id" element={<FormTema />} />
+          <Route path="/deletartema/:id" element={<DeletarTema />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
-      </AuthProvider>
+    </AuthProvider>
   );
 }
 
